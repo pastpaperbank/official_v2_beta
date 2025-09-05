635d6c755f5632f1327e7cabe71883b6c2a05000
Marlin Soe <marlin@Marlins-MacBook-Air.local>
2025-09-05 22:07:19 +0630

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from './pages/Home'
+import './App.css';
+import Home from './pages/Home';
 import ComputerSciencePage from "./pages/subjects/ComputerSciencePage";
 import ChemsitryPage from "./pages/subjects/ChemistryPage";
 import PhysicsPage from "./pages/subjects/PhysicsPage";
@@ -10,27 +10,21 @@ import FurtherPureMathPage from "./pages/subjects/FurtherPureMathPage";
 import MathBPage from "./pages/subjects/MathBPage";
 import BusinessPage from "./pages/subjects/BusinessPage";
 
-
 function App() {
-  
-
   return (
-    <>
-    <Router>
+    <Router basename="/official_v2_beta">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/computer-science" element={<ComputerSciencePage />} />
         <Route path="/chemistry" element={<ChemsitryPage />} />
         <Route path="/physics" element={<PhysicsPage />} />
         <Route path="/biology" element={<BiologyPage />} />
-        <Route path="/further-pure-math" element={<FurtherPureMathPage/>} />
+        <Route path="/further-pure-math" element={<FurtherPureMathPage />} />
         <Route path="/math-b" element={<MathBPage />} />
         <Route path="/business" element={<BusinessPage />} />
       </Routes>
     </Router>
-      
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
